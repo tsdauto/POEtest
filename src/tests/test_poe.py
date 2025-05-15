a006b71be6c0106e9c50cd2ac85872ed4fb21cd6
tsdauto <94086794+tsdauto@users.noreply.github.com>
2025-05-15 07:36:16 +0000

    chore: format code with black

--- src/tests/test_poe.py
+++ src/tests/test_poe.py
@@ -12,13 +12,14 @@ def test_poe_all(request, serial_env, telnet_env):
     from ..command.usecases.PE6108A import run as reboot_dut
     from ..command.usecases.RT_PoE5 import run2 as get_RT_PoE5_Used_w
     from ..command.usecases.RT_PoE5 import run3 as off_RT_PoE5
-    #重開DUT
+
+    # 重開DUT
     reboot_dut(telnet_env)
-    #開啟RT_PoE5
+    # 開啟RT_PoE5
     on_RT_PoE5(serial_env)
-    #讀取DUT CLI poe Used(W)
+    # 讀取DUT CLI poe Used(W)
     result1 = get_dut_Used_w(serial_env)
-    #讀取RT_PoE5 Used(W)
+    # 讀取RT_PoE5 Used(W)
     result2 = get_RT_PoE5_Used_w(serial_env)
     allure.attach(result1, name=f"供電測試結果1", attachment_type=allure.attachment_type.TEXT)
     allure.attach(result2, name=f"吃電測試結果2", attachment_type=allure.attachment_type.TEXT)
@@ -32,5 +33,5 @@ def test_poe_all(request, serial_env, telnet_env):
     allure.attach("Used(W):" + result, name=f"web測試結果3", attachment_type=allure.attachment_type.TEXT)
     value = float(result)
     assert 2.0 <= value <= 2.1
-    #關閉RT_PoE5
+    # 關閉RT_PoE5
     off_RT_PoE5(serial_env)
